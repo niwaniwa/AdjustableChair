2b36ccd1de4e6da21d593b40e324ac37979d335e
niwaniwa <konya.kokekokko@gmail.com>
2021-05-23 17:25:59 +0900

    AdjustableChair実装

---
+++ Assets/Kinel/AdjustableChair/Udon/AdjustableChair.cs
@@ -0,0 +1,75 @@
+﻿
+using System;
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDK3.Components;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class AdjustableChair : UdonSharpBehaviour
+{
+
+    [SerializeField] private Collider handleCollider;
+    [SerializeField] private MeshRenderer handle;
+    [SerializeField] private SceneSmoothShift smoothShift;
+    //[SerializeField] private Text text;
+    [SerializeField] private Transform[] objects;
+
+    public void FixedUpdate()
+    {
+        //text.text = $"{objects[0].name} : Owner ? {Networking.LocalPlayer.IsOwner(objects[0].gameObject)}";
+    }
+
+    public override void Interact()
+    {
+        Networking.LocalPlayer.UseAttachedStation();
+    }
+    
+
+    public override void OnStationEntered(VRCPlayerApi player)
+    {
+        if (Networking.LocalPlayer != player)
+            return;
+        
+        Debug.Log("[DEBUG] start " );
+        SetOwnerGameObjects(objects, player);
+        
+        if(handleCollider != null)
+            handleCollider.enabled = true;
+
+        if (handle != null)
+            handle.enabled = true;
+        
+        smoothShift.enabled = true;
+    }
+
+    private void SetOwnerGameObjects(Transform[] trans, VRCPlayerApi player)
+    {
+        for (int i = 0; i < objects.Length; i++)
+        {
+            Networking.SetOwner(player, objects[i].gameObject);
+            Debug.Log("[DEBUG] SetOwner " );
+        }
+    }
+
+    public override void OnStationExited(VRCPlayerApi player)
+    {
+        if (Networking.LocalPlayer != player)
+            return;
+        
+        if(handleCollider != null)
+            handleCollider.enabled = false;
+
+        if (handle != null)
+            handle.enabled = false;
+        
+        smoothShift.enabled = false;
+
+        var pickup = (VRCPickup) handleCollider.gameObject.GetComponent(typeof(VRCPickup));
+        if(pickup != null)
+            pickup.Drop();
+    }
+
+
+}
---
+++ Assets/Kinel/AdjustableChair/Udon/SceneSmoothShift.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class SceneSmoothShift : UdonSharpBehaviour
+{
+
+    [SerializeField] private Transform shiftStart, shiftEnd;
+    [SerializeField] private float ShiftSpeed;
+
+    private float distance = 0;
+    
+    public override void OnPlayerJoined(VRCPlayerApi player)
+    {
+        if(player == Networking.LocalPlayer)
+            distance = Vector3.Distance(shiftStart.position, shiftEnd.position);
+    }
+
+    public void FixedUpdate()
+    {
+        var present_Location = ShiftSpeed != 0 ? (Time.time * ShiftSpeed) / distance : 0;
+        transform.position = Vector3.Lerp(shiftStart.position, shiftEnd.position, present_Location);
+    }
+}
